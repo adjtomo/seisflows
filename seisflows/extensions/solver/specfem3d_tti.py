5b58c8be99cdcb9633974d08410a711e64b690a2
rmodrak <rmodrak@princeton.edu>
2014-10-03 18:17:55 -0400

    added specfem3d_tti

---
+++ seisflows/extensions/solver/specfem3d_tti.py
@@ -0,0 +1,42 @@
+
+from seisflows.tools.configure import getclass, ParameterObject
+
+PAR = ParameterObject('parameters')
+PATH = ParameterObject('paths')
+
+system = getclass('system',PAR.SYSTEM)()
+
+
+class specfem3d_thomsen(getclass('extensions.solver','specfem3d_legacy')):
+
+    # model parameters expected by solver
+    model_parameters = []
+    model_parameters += ['rho']
+    model_parameters += ['vp']
+    model_parameters += ['vs']
+    model_parameters += ['epsilon']
+    model_parameters += ['delta']
+    model_parameters += ['gamma']
+    model_parameters += ['theta']
+    model_parameters += ['azimuth']
+
+    # model parameters included in inversion
+    inversion_parameters = []
+    inversion_parameters += ['vp']
+    inversion_parameters += ['vs']
+    inversion_parameters += ['epsilon']
+    inversion_parameters += ['delta']
+    inversion_parameters += ['gamma']
+    inversion_parameters += ['theta']
+    inversion_parameters += ['azimuth']
+
+    kernel_dict = {
+        'rho':'rho_kernel',
+        'vp':'alpha_kernel',
+        'vs':'beta_kernel',
+        'epsilon':'epsilon_kernel',
+        'delta':'delta_kernel',
+        'gamma':'gamma_kernel',
+        'theta':'theta_kernel',
+        'azimuth':'azimuth_kernel'}
+
