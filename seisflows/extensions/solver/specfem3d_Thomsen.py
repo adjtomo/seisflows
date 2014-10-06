903dcd26195e5a25ff7648db8b6a804e0a081f7d
rmodrak <rmodrak@princeton.edu>
2014-10-06 14:44:15 -0400

    added Thomen, ChenTromp solver extensions

--- seisflows/extensions/solver/specfem3d_Thomsen.py
+++ seisflows/extensions/solver/specfem3d_Thomsen.py
@@ -7,7 +7,7 @@ PATH = ParameterObject('paths')
 system = getclass('system',PAR.SYSTEM)()
 
 
-class specfem3d_thomsen(getclass('extensions.solver','specfem3d_legacy')):
+class specfem3d_Thomsen(getclass('extensions.solver','specfem3d_legacy')):
 
     # model parameters expected by solver
     model_parameters = []
@@ -17,20 +17,31 @@ class specfem3d_thomsen(getclass('extensions.solver','specfem3d_legacy')):
     model_parameters += ['epsilon']
     model_parameters += ['delta']
     model_parameters += ['gamma']
+    model_parameters += ['theta']
+    model_parameters += ['azimuth']
 
     # model parameters included in inversion
     inversion_parameters = []
+    model_parameters += ['rho']
     inversion_parameters += ['vp']
     inversion_parameters += ['vs']
     inversion_parameters += ['epsilon']
     inversion_parameters += ['delta']
     inversion_parameters += ['gamma']
+    inversion_parameters += ['theta']
+    inversion_parameters += ['azimuth']
 
-    kernel_dict = {
+    # data channels
+    channels = []
+    channels += ['x']
+
+    kernel_map = {
         'rho':'rho_kernel',
         'vp':'alpha_kernel',
         'vs':'beta_kernel',
         'epsilon':'epsilon_kernel',
         'delta':'delta_kernel',
-        'gamma':'gamma_kernel'}
+        'gamma':'gamma_kernel',
+        'theta':'theta_kernel',
+        'azimuth':'azimuth_kernel'}
 
