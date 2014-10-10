3934453c75350ffc3b388aa69782354bfb694e3a
rmodrak <rmodrak@princeton.edu>
2014-10-10 10:49:27 -0400

    merged tools.signal, seistools.signal

--- seisflows/seistools/signal.py
+++ seisflows/seistools/signal.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-from seisflows.tools import signal
+import scipy.signal as signal
 
 
 def sbandpass(s,h,freqlo,freqhi):
@@ -10,7 +10,7 @@ def sbandpass(s,h,freqlo,freqhi):
     fs = 1/dt
 
     for ir in range(0,nr):
-        s[:,ir] = signal.bandpass(s[:,ir],freqlo,freqhi,fs)
+        s[:,ir] = bandpass(s[:,ir],freqlo,freqhi,fs)
     return s
 
 
@@ -80,10 +80,55 @@ def swindow(s,h,tmin,tmax,alpha=0.05,units='samples'):
         itmin = int(tmin)
         itmax = int(tmax)
 
-    win = signal.tukeywin(nt,itmin,itmax,alpha)
+    win = tukeywin(nt,itmin,itmax,alpha)
 
     # apply window
     for ir in range(0,nr):
         s[:,ir] = win*s[:,ir]
 
     return s
+
+
+
+###
+
+def correlate(u,v):
+    w = np.convolve(u,np.flipud(v))
+    return
+
+
+def bandpass(w,freqlo,freqhi,fs,npass=2):
+    wn = [2*freqlo/fs,2*freqhi/fs]
+    (b,a) = signal.butter(npass,wn)
+    w = signal.filtfilt(b,a,w)
+    return w
+
+
+def highpass(w,freq,df,npass=2):
+    raise Exception('Not yet implemented.')
+
+
+def lowpass(w,freq,df,npass=2):
+    raise Exception('Not yet implemented.')
+
+
+def window(nt,type='sine',**kwargs):
+    if type=='sine':
+        return np.sin(np.linspace(0,1,2*length))
+    elif type=='tukey':
+        return tukeywin
+
+
+def tukeywin(nt,imin,imax,alpha=0.05):
+    t = np.linspace(0,1,imax-imin)
+    w = np.zeros(imax-imin)
+    p = alpha/2.
+    lo = np.floor(p*(imax-imin-1))+1
+    hi = imax-imin-lo
+    w[:lo] = (1+np.cos(np.pi/p*(t[:lo]-p)))/2
+    w[lo:hi] = np.ones((hi-lo))
+    w[hi:] = (1+np.cos(np.pi/p*(t[hi:]-p)))/2
+    win = np.zeros(nt)
+    win[imin:imax] = w
+    return win
+
