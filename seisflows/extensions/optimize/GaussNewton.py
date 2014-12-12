2bdc69b6d419f6b3fd951801cde8b7115f3af049
Matthieu Lefebvre <ml15@princeton.edu>
2014-12-12 14:36:48 -0500

    PEP8 fixes

--- seisflows/extensions/optimize/GaussNewton.py
+++ seisflows/extensions/optimize/GaussNewton.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 from seisflows.tools import unix
@@ -11,30 +10,27 @@ PAR = ParameterObj('parameters')
 PATH = ParameterObj('paths')
 
 
-
-class GaussNewton(loadclass('optimize','default')):
+class GaussNewton(loadclass('optimize', 'default')):
     """ Implements truncated Gauss-Newton algorithm
     """
 
     def __init__(cls):
-        super(GaussNewton,cls).__init__()
+        super(GaussNewton, cls).__init__()
 
         if PAR.SCHEME != 'GaussNewton':
-            setattr(PAR,'SCHEME','GaussNewton')
+            setattr(PAR, 'SCHEME', 'GaussNewton')
 
         if 'LCGMAX' not in PAR:
-            setattr(PAR,'LCGMAX',2)
+            setattr(PAR, 'LCGMAX', 2)
 
         if 'LCGTHRESH' not in PAR:
-            setattr(PAR,'LCGTHRESH',np.inf)
-
+            setattr(PAR, 'LCGTHRESH', np.inf)
 
     def setup(cls):
         """ description goes here
         """
-        super(Newton,cls).setup()
-        cls.LCG = lib.LCG(cls.path,PAR.LCGTHRESH,PAR.LCGMAX)
-
+        super(Newton, cls).setup()
+        cls.LCG = lib.LCG(cls.path, PAR.LCGTHRESH, PAR.LCGMAX)
 
     def initialize_newton(cls):
         """ description goes here
@@ -45,8 +41,7 @@ class GaussNewton(loadclass('optimize','default')):
         m = loadnpy('m_new')
         p = cls.LCG.initialize()
         cls.delta = 1.e-3/max(abs(p))
-        savenpy('m_lcg',m+p*cls.delta)
-
+        savenpy('m_lcg', m + p*cls.delta)
 
     def update_newton(cls):
         """ description goes here
@@ -56,16 +51,16 @@ class GaussNewton(loadclass('optimize','default')):
         g0 = loadnpy('g_new')
         dg = loadnpy('g_lcg')
 
-        p,isdone = cls.LCG.update(dg/cls.delta)
+        p, isdone = cls.LCG.update(dg/cls.delta)
 
         if isdone:
-            savenpy('p_new',p)
-            savetxt('s_new',cls.delta*np.dot(g0,p))
+            savenpy('p_new', p)
+            savetxt('s_new', cls.delta*np.dot(g0, p))
 
         else:
             m = loadnpy('m_new')
             cls.delta = 1.e-3/max(abs(p))
-            savenpy('m_lcg',m+p*cls.delta)
+            savenpy('m_lcg', m + p*cls.delta)
 
         return isdone
 
--- seisflows/extensions/optimize/Newton.py
+++ seisflows/extensions/optimize/Newton.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 from seisflows.tools import unix
@@ -11,28 +10,25 @@ PAR = ParameterObj('parameters')
 PATH = ParameterObj('paths')
 
 
-
-class Newton(loadclass('optimize','default')):
+class Newton(loadclass('optimize', 'default')):
     """ Implements truncated Newton algorithm
     """
 
     def __init__(cls):
-        super(Newton,cls).__init__()
+        super(Newton, cls).__init__()
 
         if PAR.SCHEME != 'Newton':
-            setattr(PAR,'SCHEME','Newton')
+            setattr(PAR, 'SCHEME', 'Newton')
 
         if 'LCGMAX' not in PAR:
-            setattr(PAR,'LCGMAX',2)
+            setattr(PAR, 'LCGMAX', 2)
 
         if 'LCGTHRESH' not in PAR:
-            setattr(PAR,'LCGTHRESH',np.inf)
-
+            setattr(PAR, 'LCGTHRESH', np.inf)
 
     def setup(cls):
-        super(Newton,cls).setup()
-        cls.LCG = lib.LCG(cls.path,PAR.LCGTHRESH,PAR.LCGMAX)
-
+        super(Newton, cls).setup()
+        cls.LCG = lib.LCG(cls.path, PAR.LCGTHRESH, PAR.LCGMAX)
 
     def initialize_newton(cls):
         """ description goes here
@@ -43,8 +39,7 @@ class Newton(loadclass('optimize','default')):
         m = loadnpy('m_new')
         p = cls.LCG.initialize()
         cls.delta = 1.e-3/max(abs(p))
-        savenpy('m_lcg',m+p*cls.delta)
-
+        savenpy('m_lcg', m + p*cls.delta)
 
     def update_newton(cls):
         """ description goes here
@@ -54,15 +49,15 @@ class Newton(loadclass('optimize','default')):
         g0 = loadnpy('g_new')
         g = loadnpy('g_lcg')
 
-        p,isdone = cls.LCG.update((g-g0)/cls.delta)
+        p, isdone = cls.LCG.update((g - g0)/cls.delta)
 
         if isdone:
-            savenpy('p_new',p)
-            savetxt('s_new',cls.delta*np.dot(g0,p))
+            savenpy('p_new', p)
+            savetxt('s_new', cls.delta*np.dot(g0, p))
         else:
             m = loadnpy('m_new')
             cls.delta = 1.e-3/max(abs(p))
-            savenpy('m_lcg',m+p*cls.delta)
+            savenpy('m_lcg', m + p*cls.delta)
 
         return isdone
 
