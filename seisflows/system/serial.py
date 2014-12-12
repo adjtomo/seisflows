70ae01102e666a1211c9c3377255d02a540f5dde
Matthieu Lefebvre <ml15@princeton.edu>
2014-12-12 12:06:22 -0500

    PEP8 fixes

--- seisflows/system/serial.py
+++ seisflows/system/serial.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import sys
 
@@ -34,41 +33,38 @@ class serial(object):
         """
 
         if 'TITLE' not in PAR:
-            setattr(PAR,'TITLE',unix.basename(abspath('.')))
+            setattr(PAR, 'TITLE', unix.basename(abspath('.')))
 
         if 'SUBTITLE' not in PAR:
-            setattr(PAR,'SUBTITLE',unix.basename(abspath('..')))
-
+            setattr(PAR, 'SUBTITLE', unix.basename(abspath('..')))
 
         # check parameters
         if 'NTASK' not in PAR:
-            setattr(PAR,'NTASK',1)
+            setattr(PAR, 'NTASK', 1)
 
         if 'NPROC' not in PAR:
-            setattr(PAR,'NPROC',1)
+            setattr(PAR, 'NPROC', 1)
 
         if 'VERBOSE' not in PAR:
-            setattr(PAR,'VERBOSE',1)
-
+            setattr(PAR, 'VERBOSE', 1)
 
         # check paths
         if 'GLOBAL' not in PATH:
-            setattr(PATH,'GLOBAL',join(abspath('.'),'scratch'))
+            setattr(PATH, 'GLOBAL', join(abspath('.'), 'scratch'))
 
         if 'LOCAL' not in PATH:
-            setattr(PATH,'LOCAL','')
+            setattr(PATH, 'LOCAL', '')
 
         if 'SUBMIT' not in PATH:
-            setattr(PATH,'SUBMIT',unix.pwd())
+            setattr(PATH, 'SUBMIT', unix.pwd())
 
         if 'OUTPUT' not in PATH:
-            setattr(PATH,'OUTPUT',join(PATH.SUBMIT,'output'))
+            setattr(PATH, 'OUTPUT', join(PATH.SUBMIT, 'output'))
 
         if 'SYSTEM' not in PATH:
-            setattr(PATH,'SYSTEM',join(PATH.GLOBAL,'system'))
+            setattr(PATH, 'SYSTEM', join(PATH.GLOBAL, 'system'))
 
-
-    def submit(self,workflow):
+    def submit(self, workflow):
         """ Submits job
         """
         unix.mkdir(PATH.OUTPUT)
@@ -80,45 +76,40 @@ class serial(object):
 
         workflow.main()
 
-
-    def run(self,classname,funcname,hosts='all',**kwargs):
+    def run(self, classname, funcname, hosts='all', **kwargs):
         """ Runs tasks in serial or parallel on specified hosts
         """
         unix.mkdir(PATH.SYSTEM)
 
-        if hosts=='all':
+        if hosts == 'all':
             for itask in range(PAR.NTASK):
                 self.setnode(itask)
                 self.progress(itask)
-                func = getattr(__import__(classname),funcname)
+                func = getattr(__import__(classname), funcname)
                 func(**kwargs)
             print ''
 
-        elif hosts=='head':
+        elif hosts == 'head':
             self.setnode(0)
-            func = getattr(__import__(classname),funcname)
+            func = getattr(__import__(classname), funcname)
             func(**kwargs)
 
         else:
             task(**kwargs)
 
-
     def getnode(self):
         """Gets number of running task"""
-        return int(np.loadtxt(PATH.SYSTEM+'/'+'nodenum'))
+        return int(np.loadtxt(PATH.SYSTEM + '/' + 'nodenum'))
 
-
-    def setnode(self,itask):
+    def setnode(self, itask):
         """Sets number of running task"""
-        np.savetxt(PATH.SYSTEM+'/'+'nodenum',[itask])
-
+        np.savetxt(PATH.SYSTEM + '/' + 'nodenum', [itask])
 
     def mpiexec(self):
         """Wrapper for mpiexec"""
         return 'mpiexec -np %d ' % PAR.NPROC
 
-
-    def progress(self,itask=None):
+    def progress(self, itask=None):
         """Prints status updates"""
         if PAR.VERBOSE and PAR.NTASK > 1:
-            print ' task '+'%02d'%(itask+1)+' of '+'%02d'%PAR.NTASK
+            print ' task ' + '%02d' % (itask + 1) + ' of ' + '%02d' % PAR.NTASK
