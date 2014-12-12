3899acf2b24f10fd9578db5c610f7a7a0e01e0f8
Matthieu Lefebvre <ml15@princeton.edu>
2014-12-12 14:16:18 -0500

    PEP8 fixes

--- seisflows/postprocess/default.py
+++ seisflows/postprocess/default.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 from seisflows.tools import unix
@@ -36,58 +35,56 @@ class default(object):
         global system
         import system
 
-
         # check postprocessing settings
         if 'PRECOND' not in PATH:
-            setattr(PATH,'PRECOND',None)
+            setattr(PATH, 'PRECOND', None)
 
         if 'SMOOTH' not in PAR:
-            setattr(PAR,'SMOOTH',0.)
+            setattr(PAR, 'SMOOTH', 0.)
 
         if 'SCALE' not in PAR:
-            setattr(PAR,'SCALE',1.)
+            setattr(PAR, 'SCALE', 1.)
 
-
-    def process_kernels(self,tag='grad',path=None,optim_path=None):
+    def process_kernels(self, tag='grad', path=None, optim_path=None):
         """ Computes gradient and performs smoothing, preconditioning, and then
             scaling operations
         """
-        assert(exists(path))
+        assert (exists(path))
 
         # combine kernels
-        system.run('solver','combine',
-            hosts='head',
-            path=path+'/'+'kernels')
+        system.run('solver', 'combine',
+                   hosts='head',
+                   path=path + '/' + 'kernels')
 
         # write gradient
-        unix.cd(path+'/'+'kernels')
-        g = solver.merge(solver.load('sum',type='kernel'))
-        m = solver.merge(solver.load('../model',type='model'))
+        unix.cd(path + '/' + 'kernels')
+        g = solver.merge(solver.load('sum', type='kernel'))
+        m = solver.merge(solver.load('../model', type='model'))
         g *= m
-        solver.save(path+'/'+tag,solver.split(g))
+        solver.save(path + '/' + tag, solver.split(g))
 
         # apply smoothing
         if PAR.SMOOTH > 0.:
-            system.run('solver','smooth',
-                hosts='head',
-                path=path,
-                span=PAR.SMOOTH)
+            system.run('solver', 'smooth',
+                       hosts='head',
+                       path=path,
+                       span=PAR.SMOOTH)
 
         # apply preconditioner
         if PATH.PRECOND:
             unix.cd(path)
             g = solver.merge(solver.load(tag))
             p = solver.merge(solver.load(PATH.PRECOND))
-            unix.mv(tag,tag+'_noscale')
-            solver.save(tag,solver.split(g/p))
+            unix.mv(tag, tag + '_noscale')
+            solver.save(tag, solver.split(g/p))
 
         # apply scaling
         if PAR.SCALE:
             unix.cd(path)
-            g = solver.merge(solver.load(tag,type='model'))
+            g = solver.merge(solver.load(tag, type='model'))
             g *= PAR.SCALE
-            solver.save(tag,solver.split(g))
+            solver.save(tag, solver.split(g))
 
         if optim_path:
-            savenpy(optim_path,g)
+            savenpy(optim_path, g)
 
