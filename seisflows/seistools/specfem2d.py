b0af4e0a24ccf352287a255d8ab0e80e36450d94
rmodrak <rmodrak@princeton.edu>
2015-02-09 21:05:41 -0500

    refactored seistools

--- seisflows/seistools/specfem2d.py
+++ seisflows/seistools/specfem2d.py
@@ -1,10 +1,6 @@
 
-import string as _string
-import numpy as _np
-
-from seisflows.tools import unix
-from seisflows.tools.code import Struct
 from seisflows.tools.config import findpath
+from seisflows.seistools.shared import getpar, setpar
 
 
 ### input file writers
@@ -105,51 +101,6 @@ def write_parameters(par, version='git-devel'):
     _writelines(file, lines)
 
 
-def getpar(key, file='DATA/Par_file', sep='='):
-    """ Reads parameter from parfile
-    """
-    with open(file, 'r') as f:
-
-        # read line by line
-        for line in f:
-            if _string.find(line, key) == 0:
-                # read key
-                key, val = _split(line, sep)
-                if not key:
-                    continue
-                # read val
-                val, _ = _split(val, '#')
-                return val.strip()
-
-        raise Exception
-
-
-def setpar(key, val, file='DATA/Par_file', path='.', sep='='):
-    """ Writes parameter to parfile
-    """
-
-    val = str(val)
-
-    # read line by line
-    with open(path + '/' + file, 'r') as f:
-        lines = []
-        for line in f:
-            if _string.find(line, key) == 0:
-                # read key
-                key, _ = _split(line, sep)
-                # read comment
-                _, comment = _split(line, '#')
-                n = len(line) - len(key) - len(val) - len(comment) - 2
-                # replace line
-                if comment:
-                    line = _merge(key, sep, val, ' '*n, '#', comment)
-                else:
-                    line = _merge(key, sep, str(val), '\n')
-            lines.append(line)
-
-    # write file
-    _writelines(path + '/' + file, lines)
-
 
 ### utility functions
 
@@ -158,34 +109,4 @@ def _writelines(file, lines):
     """
     with open(file, 'w') as f:
         f.writelines(lines)
-
-
-def _split(str, sep):
-    from string import find
-
-    n = find(str, sep)
-    if n >= 0:
-        return str[:n], str[n + len(sep):]
-    else:
-        return str, ''
-
-
-def _merge(*parts):
-    return ''.join(parts)
-
-
-def _cmp(names):
-    rank = []
-    for name in names:
-        ii = int(unix.basename(name).split('_')[0])
-        rank.append(ii)
-    print rank
-    return [names[ii] for ii in rank]
-
-
-def _stack(a1, a2):
-    if a1.size > 0:
-        return _np.column_stack((a1, a2))
-    else:
-        return a2
 
