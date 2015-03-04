f180bbbd8662ce31f99b6052caf41ecc080d6563
rmodrak <rmodrak@princeton.edu>
2015-03-04 11:01:12 -0500

    updated system integration test

--- tests/integration/system/parameters.py
+++ tests/integration/system/parameters.py
@@ -1,4 +1,12 @@
-SYSTEM = 'serial'
+SYSTEM = 'slurm_lg'
 NPROC = 1
 NTASK = 16
+NPROC_PER_NODE = 16
+
+WORKFLOW=None        # not used
+SOLVER=None          # not used
+OPTIMIZE=None        # not used
+PREPROCESS=None      # not used
+POSTPROCESS=None     # not used
+
 
