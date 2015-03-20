c48ae6947883e17e0320efe380844c20c9bc0566
rmodrak <rmodrak@princeton.edu>
2015-03-20 17:25:29 -0400

    tweaked lib.LBFGS

--- seisflows/optimize/lib/LBFGS.py
+++ seisflows/optimize/lib/LBFGS.py
@@ -1,3 +1,4 @@
+
 import os
 import pickle
 import time
@@ -5,6 +6,8 @@ import time
 import numpy as np
 
 from seisflows.tools import unix
+from seisflows.tools.array import loadnpy, savenpy
+from seisflows.tools.code import loadtxt, savetxt
 
 
 class LBFGS:
@@ -12,58 +15,53 @@ class LBFGS:
     """
 
     def __init__(self, path='.', kmax=5, iter=1):
-
         self.path = path
-        self.load = load
-        self.save = save
         self.kmax = kmax
 
-        unix.mkdir(self.path + '/' + 'LBFGS')
-        unix.cd(self.path + '/' + 'LBFGS')
+        unix.cd(self.path)
+        unix.mkdir('LBFGS')
 
         if iter == 1:
-            savetxt('k', 0)
+            savetxt('LBFGS/k', 0)
+
 
     def update(self):
-
         unix.cd(self.path)
-        s = self.load('m_new') - self.load('m_old')
-        y = self.load('g_new') - self.load('g_old')
+        s = loadnpy('m_new') - loadnpy('m_old')
+        y = loadnpy('g_new') - loadnpy('g_old')
         n = len(s)
 
-        unix.cd('LBFGS')
-        k = loadtxt('k')
-        k = min(k + 1, self.kmax)
+        k = int(loadtxt('LBFGS/k'))
+        k = min(k+1, self.kmax)
 
         if k == 1:
-            S = np.memmap('S', mode='w+', dtype='float32', shape=(n, self.kmax))
-            Y = np.memmap('Y', mode='w+', dtype='float32', shape=(n, self.kmax))
+            S = np.memmap('LBFGS/S', mode='w+', dtype='float32', shape=(n, self.kmax))
+            Y = np.memmap('LBFGS/Y', mode='w+', dtype='float32', shape=(n, self.kmax))
             S[:, 0] = s
             Y[:, 0] = y
 
         else:
-            S = np.memmap('S', mode='r+', dtype='float32', shape=(n, self.kmax))
-            Y = np.memmap('Y', mode='r+', dtype='float32', shape=(n, self.kmax))
+            S = np.memmap('LBFGS/S', mode='r+', dtype='float32', shape=(n, self.kmax))
+            Y = np.memmap('LBFGS/Y', mode='r+', dtype='float32', shape=(n, self.kmax))
             S[:, 1:] = S[:, :-1]
             Y[:, 1:] = Y[:, :-1]
             S[:, 0] = s
             Y[:, 0] = y
 
-        savetxt('k', k)
+        savetxt('LBFGS/k', k)
         del S
         del Y
 
-    def solve(self):
 
+    def solve(self):
         unix.cd(self.path)
-        g = self.load('g_new')
+        g = loadnpy('g_new')
         q = np.copy(g)
         n = len(q)
 
-        unix.cd('LBFGS')
-        S = np.memmap('S', mode='r', dtype='float32', shape=(n, self.kmax))
-        Y = np.memmap('Y', mode='r', dtype='float32', shape=(n, self.kmax))
-        k = loadtxt('k')
+        S = np.memmap('LBFGS/S', mode='r', dtype='float32', shape=(n, self.kmax))
+        Y = np.memmap('LBFGS/Y', mode='r', dtype='float32', shape=(n, self.kmax))
+        k = int(loadtxt('LBFGS/k'))
 
         rh = np.zeros(k)
         al = np.zeros(k)
@@ -93,33 +91,14 @@ class LBFGS:
 
 
     def restart(self):
-
         print 'restarting LBFGS...'
         time.sleep(2)
 
-        unix.cd(self.path + '/' + 'LBFGS')
-        savetxt('k', 0)
-        S = np.memmap('S', mode='r+')
-        Y = np.memmap('Y', mode='r+')
+        unix.cd(self.path)
+        savetxt('LBFGS/k', 0)
+        S = np.memmap('LBFGS/S', mode='r+')
+        Y = np.memmap('LBFGS/Y', mode='r+')
         S[:] = 0.
         Y[:] = 0.
 
-
-# utility functions
-
-def loadtxt(filename):
-    return int(np.loadtxt(filename))
-
-
-def savetxt(filename, v):
-    np.savetxt(filename, [v], '%d')
-
-
-def load(filename):
-    return np.load(filename)
-
-
-def save(filename, v):
-    np.save(filename, v)
-    unix.mv(filename + '.npy', filename)
 
