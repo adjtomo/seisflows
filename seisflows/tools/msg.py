d1386fbc91d343c83d5739d29e4a6a73c4c3faad
rmodrak <rmodrak@princeton.edu>
2015-03-24 20:50:54 -0400

    added tools.msg

---
+++ seisflows/tools/msg.py
@@ -0,0 +1,26 @@
+
+
+###
+
+MsgTaskError = """
+TASK ERROR
+
+    Task failed:  %s.%s
+
+    For more information, see output.slurm/%s
+
+    Stopping workflow...
+"""
+
+
+###
+
+MsgSourceInputError = """
+SOURCE INPUT ERROR
+
+    In DIRECTORY, there must be one or more files matching WILDCARD.
+
+    DIRECTORY:  "%s"
+    WILDCARD:  "%s"
+"""
+
