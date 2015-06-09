413ccd62e3de504832ea9b29187b66b8d5313d64
rmodrak <rmodrak@princeton.edu>
2015-06-09 16:49:04 -0400

    refactored lib.NLCG

--- seisflows/optimize/lib/NLCG.py
+++ seisflows/optimize/lib/NLCG.py
@@ -4,76 +4,59 @@ import numpy as np
 
 from seisflows.tools import unix
 
+from seisflows.tools.array import loadnpy, savenpy
+from seisflows.tools.code import loadtxt, savetxt
+
+
 
 class NLCG:
     """ Nonlinear conjugate gradient method
     """
 
-    def __init__(self, path, thresh, itercgmax):
+    def __init__(self, path, thresh, itermax):
 
         self.path = path
         unix.mkdir(self.path+'/'+'NLCG')
 
-        self.load = load
-        self.save = save
-
-        self.itercgmax = itercgmax
+        self.itermax = itermax
         self.thresh = thresh
 
         try:
-            self.itercg = loadtxt(self.path+'/'+'NLCG/itercg')
+            self.iter = loadtxt(self.path+'/'+'NLCG/iter')
         except IOError:
-            self.itercg = 0
+            self.iter = 0
+
 
     def compute(self):
-
-        # utility functions
-
-        def fletcher_reeves():
-            # fletcher-reeves update
-            top = np.dot(g_new, g_new)
-            bot = np.dot(g_old, g_old)
-            beta = top/bot
-            return -g_new + beta*p_old
-
-        def pollak_ribere():
-            # pollack-riebere update
-            top = np.dot(g_new, g_new-g_old)
-            bot = np.dot(g_old, g_old)
-            beta = top/bot
-            return -g_new + beta*p_old
-
-        def rho():
-            return abs(np.dot(g_new, g_old) / np.dot(g_new, g_new))
-
-
-        # algorithm starts here
-        self.itercg += 1
-        savetxt(self.path+'/'+'NLCG/itercg', self.itercg)
+        """ Given gradient, returns NLCG search direction
+        """
+        self.iter += 1
+        savetxt(self.path+'/'+'NLCG/iter', self.iter)
 
         unix.cd(self.path)
-        g_new = self.load('g_new')
+        g_new = loadnpy('g_new')
 
-        if self.itercg == 1:
+        if self.iter == 1:
             return -g_new
 
-        if self.itercg > self.itercgmax:
+        elif self.iter > self.itermax:
             print 'restarting NLCG... [periodic restart]'
             self.restart()
             return -g_new
 
-        # compute NLCG direction
-        g_old = self.load('g_old')
-        p_old = self.load('p_old')
-        p_new = pollak_ribere()
+        # compute search direction
+        g_old = loadnpy('g_old')
+        p_old = loadnpy('p_old')
+        beta = pollak_ribere(g_new, g_old)
+        p_new = -g_new + beta*p_old
 
         # check restart conditions
-        if rho() > self.thresh:
+        if check_conjugacy(g_new, g_old) > self.thresh:
             print 'restarting NLCG... [loss of conjugacy]'
             self.restart()
             return -g_new
 
-        elif np.dot(p_new, g_new) > 0:
+        elif check_descent(p_new, g_new) > 0.:
             print 'restarting NLCG... [not a descent direction]'
             self.restart()
             return -g_new
@@ -83,23 +66,33 @@ class NLCG:
 
 
     def restart(self):
-        self.itercg = 1
-        savetxt(self.path+'/'+'NLCG/itercg', self.itercg)
+        """ Restarts algorithm
+        """
+        self.iter = 1
+        savetxt(self.path+'/'+'NLCG/iter', self.iter)
 
 
 
-def loadtxt(filename):
-    return int(np.loadtxt(filename))
+### utility functions
+
+def fletcher_reeves(g_new, g_old):
+    num = np.dot(g_new, g_new)
+    den = np.dot(g_old, g_old)
+    beta = num/den
+    return beta
+
+def pollak_ribere(g_new, g_old):
+    num = np.dot(g_new, g_new-g_old)
+    den = np.dot(g_old, g_old)
+    beta = num/den
+    return beta
+
+def check_conjugacy(g_new, g_old):
+    return abs(np.dot(g_new, g_old) / np.dot(g_new, g_new))
+
+def check_descent(p_new, g_new):
+    return np.dot(p_new, g_new) / np.dot(g_new, g_new)
 
 
-def savetxt(filename, v):
-    np.savetxt(filename, [v], '%d')
 
 
-def load(filename):
-    return np.load(filename)
-
-
-def save(filename, v):
-    np.save(filename, v)
-    unix.mv(filename+'.npy', filename)
