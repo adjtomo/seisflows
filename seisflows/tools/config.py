0782d8b15a1cf8dae6a13c0f978ffc9f1b0b1730
rmodrak <rmodrak@princeton.edu>
2015-09-01 04:24:10 -0400

    customize pickle behavior via copy_reg

--- seisflows/tools/config.py
+++ seisflows/tools/config.py
@@ -1,7 +1,10 @@
-import os
+
+import copy_reg
 import imp
+import os
 import sys
 import types
+
 from os.path import abspath, join
 
 from seisflows.tools import unix
@@ -297,4 +300,25 @@ def _exists(parts):
         return True
     except ImportError:
         return False
+
+
+### customize pickle behavior
+
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
 
