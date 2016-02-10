dd10055a6980c336d0e54c9118c7689e51f95725
rmodrak <rmodrak@princeton.edu>
2016-02-10 01:22:11 -0500

    added workflow.thrifty_inversion

---
+++ seisflows/workflow/thrifty_inversion.py
@@ -0,0 +1,119 @@
+
+from os.path import join
+import sys
+import numpy as np
+
+from seisflows.tools import msg
+from seisflows.tools import unix
+from seisflows.tools.array import loadnpy, savenpy
+from seisflows.tools.code import divides, exists
+from seisflows.tools.config import SeisflowsParameters, SeisflowsPaths, \
+    ParameterError
+
+PAR = SeisflowsParameters()
+PATH = SeisflowsPaths()
+
+import system
+import solver
+import optimize
+import preprocess
+import postprocess
+
+
+class thrifty_inversion(loadclass('workflow', 'inversion')):
+    """ Thrifty inversion subclass
+
+      Provides additional savings by avoiding redundant forward simulations
+      associated with sufficient decrease and curvature tests in a safeguarded
+      backtracking line search. Otherwise, the same as regular inversion
+      workflow.
+
+      For more readable but less efficient inversion workflow, feel free to
+      revert to the parent class.
+    """
+
+    def solver_status(self, maxiter=1):
+        """ Keeps track of whether a forward simulation would be redundant
+        """
+        if optimize.iter <= maxiter:
+            # forward simulation not redundant because solver files do not exist
+            # prior to first iteration
+            return False
+
+        elif optimize.iter == PAR.BEGIN:
+            # forward simulation not redundant because solver files need to be
+            # reinstated after possible multiscale transition
+            return False
+
+        elif PATH.LOCAL:
+            # forward simulation not redundant because solver files need to be
+            # reinstated on local filesystems
+            return False
+
+        elif PAR.LINESEARCH != 'Backtrack':
+            # thrifty inversion only implemented for backtracking line search,
+            # not bracketing line search
+            return False
+
+        elif optimize.restarted:
+            # forward simulation not redundant following optimization algorithm
+            # restart
+            return False
+
+        else:
+            # if none of the above conditions are triggered, then forward 
+            # simulation is redundant, can be skipped
+            return True
+
+    def setup(self):
+        """ Lays groundwork for inversion
+        """
+        # clean scratch directories
+        if PAR.BEGIN == 1:
+            unix.rm(PATH.GLOBAL)
+            unix.mkdir(PATH.GLOBAL)
+
+            preprocess.setup()
+            postprocess.setup()
+            optimize.setup()
+
+        isready = self.solver_status()
+        if not isready:
+            system.run('solver', 'setup',
+                       hosts='all')
+
+
+    def initialize(self):
+        # are prerequisites for gradient evaluation in place?
+        isready = self.solver_status(maxiter=2)
+
+        # if not, then prepare for gradient evaluation
+        if not isready:
+            super(thrifty, self).initialize()
+
+
+    def iterate_search(self):
+        super(thrifty, self).iterate_search()
+
+        isdone = optimize.isdone
+        isready = self.solver_status()
+
+        # to avoid redundant forward simulation, save solver files associated
+        # with 'best' trial model
+        if isready and isdone:
+            unix.rm(PATH.SOLVER+'_best')
+            unix.mv(PATH.SOLVER, PATH.SOLVER+'_best')
+
+
+    def clean(self):
+        isready = self.solver_status()
+        if isready:
+            unix.rm(PATH.GRAD)
+            unix.mv(PATH.FUNC, PATH.GRAD)
+            unix.mkdir(PATH.FUNC)
+            unix.rm(PATH.SOLVER)
+            unix.mv(PATH.SOLVER+'_best', PATH.SOLVER)
+        else:
+            super(thrifty, self).clean()
+
+
