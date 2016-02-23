ca8f250eff52bdc82992d8800ed4015fe318e549
rmodrak <rmodrak@princeton.edu>
2016-02-22 19:48:34 -0500

    added literature references on objective functions

--- seisflows/seistools/misfit.py
+++ seisflows/seistools/misfit.py
@@ -1,6 +1,6 @@
 
 import numpy as _np
-from scipy.signal import hilbert as analytic
+from scipy.signal import hilbert as _analytic
 
 
 def Traveltime(syn, obs, nt, dt):
@@ -43,56 +43,48 @@ def Waveform(syn, obs, nt, dt):
 
 def Envelope(syn, obs, nt, dt, eps=0.05):
     # envelope difference
-    esyn = abs(analytic(syn))
-    eobs = abs(analytic(obs))
+    # (Yuan et al 2015, eq 9)
+    esyn = abs(_analytic(syn))
+    eobs = abs(_analytic(obs))
     ersd = esyn-eobs
     return _np.sqrt(_np.sum(ersd*ersd*dt))
-
-
-def Traveltime(syn, obs, nt, dt):
-    # cross correlation time
-    cc = abs(_np.convolve(obs, _np.flipud(syn)))
-    cmax = 0
-    misfit = 0.
-    ioff = None
-    for it in range(2*nt-1):
-        if cc[it] > cmax:
-            cmax = cc[it]
-            ioff = it
-            misfit = (ioff-nt+1)*dt
-    if ioff is not None:
-        misfit = (ioff-nt+1)*dt
-    return misfit
 
 
 def InstantaneousPhase(syn, obs, nt, dt, eps=0.05):
     # instantaneous phase 
     # from Bozdag et al. 2011
 
-    r = _np.real(analytic(syn))
-    i = _np.imag(analytic(syn))
+    r = _np.real(_analytic(syn))
+    i = _np.imag(_analytic(syn))
     phi_syn = _np.arctan2(i,r)
 
-    r = _np.real(analytic(obs))
-    i = _np.imag(analytic(obs))
+    r = _np.real(_analytic(obs))
+    i = _np.imag(_analytic(obs))
     phi_obs = _np.arctan2(i,r)
 
     phi_rsd = phi_syn - phi_obs
     return _np.sqrt(_np.sum(phi_rsd*phi_rsd*dt))
 
 
+def Envelope2(syn, obs, nt, dt, eps=0.):
+    # envelope amplitude ratio
+    # (Yuan et al 2015, eq B-1)
+    esyn = abs(_analytic(syn))
+    eobs = abs(_analytic(obs))
+    raise NotImplementedError
+
+
 def Envelope3(syn, obs, nt, dt, eps=0.):
     # envelope cross-correlation lag
-    # (Yuan et al 2015, eqs B-2, B-5)
-    esyn = abs(analytic(syn))
-    eobs = abs(analytic(obs))
+    # (Yuan et al 2015, eqs B-4)
+    esyn = abs(_analytic(syn))
+    eobs = abs(_analytic(obs))
     return Traveltime(esyn, eobs, nt, dt)
 
 
-
 def AnalyticSignal(syn, obs, nt, dt, eps=0.):
-    esyn = abs(analytic(syn))
-    eobs = abs(analytic(obs))
+    esyn = abs(_analytic(syn))
+    eobs = abs(_analytic(obs))
 
     esyn1 = esyn + eps*max(esyn)
     eobs1 = eobs + eps*max(eobs)
