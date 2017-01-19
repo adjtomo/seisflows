4b5782540b3810dfbfec9710f8afe0fd98f6a606
rmodrak <rmodrak@princeton.edu>
2017-01-19 13:26:22 -0500

    changed import order to allow optimize class to depend on other seisflows classes

--- seisflows/config.py
+++ seisflows/config.py
@@ -10,19 +10,19 @@ from importlib import import_module
 from pkgutil import find_loader
 from os.path import abspath, join, exists
 from seisflows.tools import unix
-from seisflows.tools.code import Struct, loadjson, loadobj, savejson, saveobj, loadpy
+from seisflows.tools.code import Struct, loadjson, loadobj, loadyaml, savejson, saveobj
 from seisflows.tools.msg import WarningOverwrite, ImportError1, ImportError2, ImportError3, ImportError4
 
-# SeisFlows consists of interacting 'system', 'preprocess', 'solver', 'postprocess', 'optimize', and 'workflow' objects. Each corresponds simultaneously to a module in the SeisFlows source code, a class that is instantiated and made accessible via sys.modules, and parameter in a global dictionary. Once in memory, these objects can be thought of as comprising the complete 'state' of a SeisFlows session
+# SeisFlows consists of interacting 'system', 'preprocess', 'solver', 'postprocess', 'optimize', and 'workflow' objects. Each corresponds simultaneously to a module in the SeisFlows source code, a class that is instantiated and made accessible via sys.modules, and a parameter in a global dictionary. Once in memory, these objects can be thought of as comprising the complete 'state' of a SeisFlows session
 
 # The following list is one of the few hardwired aspects of the whole SeisFlows package. Any changes may result in circular imports or other problems
 
 names = []
 names += ['system']
-names += ['optimize']
 names += ['preprocess']
 names += ['solver']
 names += ['postprocess']
+names += ['optimize']
 names += ['workflow']
 
 
