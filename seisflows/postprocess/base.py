761d171a64cd4e1dd662cbb116f7185b22eafb6f
rmodrak <rmodrak@princeton.edu>
2017-09-24 16:48:32 -0400

    refactored postprocess.base

--- seisflows/postprocess/base.py
+++ seisflows/postprocess/base.py
@@ -95,19 +95,22 @@ class base(object):
             parameters = solver.parameters
 
         if PAR.SMOOTH > 0:
-            suffix = '_nosmooth'
+            solver.combine(
+                   input_path=path,
+                   output_path=path+'/'+'sum_nosmooth',
+                   parameters=parameters)
 
-        solver.combine(
-               input_path=path,
-               output_path=path+'/'+'sum'+suffix,
-               parameters=parameters)
-
-        if PAR.SMOOTH > 0.:
             solver.smooth(
-                   input_path=path+'/'+'sum'+suffix,
+                   input_path=path+'/'+'sum_nosmooth',
                    output_path=path+'/'+'sum',
                    parameters=parameters,
                    span=PAR.SMOOTH)
+        else:
+            solver.combine(
+                   input_path=path,
+                   output_path=path+'/'+'sum',
+                   parameters=parameters)
+
 
 
     def save(self, g, path='', parameters=[], backup=None):
