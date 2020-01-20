35d26001885aeb32294598c7a839a62035f26e6e
Bryant Chow <bryant.h.c@gmail.com>
2020-01-20 13:10:08 +1300

    commented and Py3'd config.py

--- seisflows/config.py
+++ seisflows/config.py
@@ -1,24 +1,31 @@
+#!/usr/bin/env python
+"""
+SeisFlows consists of interacting objects:
+'system', 'preprocess', 'solver', 'postprocess', 'optimize', 'workflow'
 
-import copy_reg
-import imp
+Each corresponds simultaneously to a module in the SeisFlows source code,
+a class that is instantiated and made accessible via sys.modules, and a
+parameter in a global dictionary. Once in memory, these objects can be thought
+of as comprising the complete 'state' of a SeisFlows session
+"""
+
 import os
-import re
+# import re
 import sys
+# import imp
 import types
-
+import copyreg
 from importlib import import_module
-from os.path import abspath, join, exists
 
 from seisflows.tools import msg
-from seisflows.tools.err import ParameterError
+# from seisflows.tools.err import ParameterError
 from seisflows.tools import unix
-from seisflows.tools.tools import loadjson, loadobj, loadpy, savejson, saveobj
+from seisflows.tools.tools import loadjson, loadobj, savejson, saveobj
 from seisflows.tools.tools import module_exists, package_exists
 
-# SeisFlows consists of interacting 'system', 'preprocess', 'solver', 'postprocess', 'optimize', and 'workflow' objects. Each corresponds simultaneously to a module in the SeisFlows source code, a class that is instantiated and made accessible via sys.modules, and a parameter in a global dictionary. Once in memory, these objects can be thought of as comprising the complete 'state' of a SeisFlows session
 
-# The following list is one of the few hardwired aspects of the whole SeisFlows package. Any changes may result in circular imports or other problems
-
+# The following list is one of the few hardwired aspects of the whole
+# SeisFlows package. Any changes may result in circular imports, other problems
 names = []
 names += ['system']
 names += ['preprocess']
@@ -29,64 +36,71 @@ names += ['workflow']
 
 
 def config():
-    """ Instantiates SeisFlows objects and makes them globally accessible by
-      registering them in sys.modules
-   """
-    # parameters and paths must already be loaded
-    # (normally this is done by sfsubmit)
+    """
+    Instantiates SeisFlows objects and makes them globally accessible by
+    registering them in sys.modules
+    """
+    # Parameters and paths must already be loaded (normally done by sfsubmit)
     assert 'seisflows_parameters' in sys.modules
     assert 'seisflows_paths' in sys.modules
 
-    # check if objects already exist on disk
-    if exists(_output()):
-        print msg.WarningOverwrite
+    # Check if objects already exist on disk
+    if os.path.exists(_output()):
+        print(msg.WarningOverwrite)
         sys.exit()
 
-    # instantiate and register objects
+    # Instantiate and register objects
     for name in names:
-        sys.modules['seisflows_'+name] = custom_import(name)()
+        sys.modules['seisflows_' + name] = custom_import(name)()
 
-    # error checking
+    # Error checking
     for name in names:
-        sys.modules['seisflows_'+name].check()
+        sys.modules['seisflows_' + name].check()
 
-    if not hasattr(sys.modules['seisflows_parameters'], 'workflow'.upper()):
-        print msg.MissingParameter_Worfklow
+    # Ensure that certain parameters are instantiated
+    if not hasattr(sys.modules['seisflows_parameters'], "WORKFLOW"):
+        print(msg.MissingParameter_Worfklow)
         sys.exit(-1)
-
-    if not hasattr(sys.modules['seisflows_parameters'], 'system'.upper()):
-        print msg.MissingParameter_System
+    if not hasattr(sys.modules['seisflows_parameters'], "SYSTEM"):
+        print(msg.MissingParameter_System)
         sys.exit(-1)
 
 
 def save():
-    """ Exports session to disk
+    """
+    Export the current session to disk
     """
     unix.mkdir(_output())
 
+    # Save the paths and parameters into a JSON file
     for name in ['parameters', 'paths']:
-        fullfile = join(_output(), 'seisflows_'+name+'.json')
-        savejson(fullfile, sys.modules['seisflows_'+name].__dict__)
+        fullfile = os.path.join(_output(), f"seisflows_{name}.json")
+        savejson(fullfile, sys.modules['seisflows_' + name].__dict__)
 
+    # Save the current workflow as pickle objects
     for name in names:
-        fullfile = join(_output(), 'seisflows_'+name+'.p')
-        saveobj(fullfile, sys.modules['seisflows_'+name])
+        fullfile = os.path.join(_output(), f"seisflows_{name}.p")
+        saveobj(fullfile, sys.modules['seisflows_' + name])
 
 
-def load(path):
-    """ Imports session from disk
+def load():
     """
+    Imports a previously saved session from disk
+    """
+    # Load parameters and paths from a JSON file
     for name in ['parameters', 'paths']:
-        fullfile = join(_full(path), 'seisflows_'+name+'.json')
-        sys.modules['seisflows_'+name] = Dict(loadjson(fullfile))
+        fullfile = os.path.join(_output(), f"seisflows_{name}.json")
+        sys.modules['seisflows_' + name] = Dict(loadjson(fullfile))
 
+    # Load the saved workflow from pickle objects
     for name in names:
-        fullfile = join(_full(path), 'seisflows_'+name+'.p')
-        sys.modules['seisflows_'+name] = loadobj(fullfile)
+        fullfile = os.path.join(_output(), f"seisflows_{name}.p")
+        sys.modules['seisflows_' + name] = loadobj(fullfile)
 
 
 class Dict(object):
-    """ Dictionary-like object for holding parameters or paths
+    """
+    Re defined dictionary-like object for holding parameters or paths
     """
     def __iter__(self):
         return iter(sorted(self.__dict__.keys()))
@@ -115,7 +129,8 @@ class Dict(object):
 
 
 class Null(object):
-    """ Always and reliably does nothing
+    """
+    A null object that always and reliably does nothing
     """
     def __init__(self, *args, **kwargs):
         pass
@@ -137,14 +152,16 @@ class Null(object):
 
 
 def custom_import(*args):
-    """ Imports SeisFlows module and extracts class of same name. For example,
+    """
+    Imports SeisFlows module and extracts class of same name.
 
-            custom_import('workflow', 'inversion') 
+    For example:
+        custom_import('workflow', 'inversion')
 
         imports 'seisflows.workflow.inversion' and, from this module, extracts
         class 'inversion'.
     """
-    # parse input arguments
+    # Parse input arguments
     if len(args) == 0:
         raise Exception(msg.ImportError1)
     if args[0] not in names:
@@ -154,75 +171,126 @@ def custom_import(*args):
     if not args[1]:
         return Null
 
-    # generate package list
+    # Generate package list
     packages = ['seisflows']
 
-    # does module exist?
+    # Check if modules exist
     _exists = False
     for package in packages:
-        full_dotted_name = package+'.'+args[0]+'.'+args[1]
+        full_dotted_name = f"{package}.{args[0]}.{args[1]}"
         if module_exists(full_dotted_name):
             _exists = True
             break
     if not _exists:
-        raise Exception(msg.ImportError3 % 
-            (args[0], args[1], args[0].upper()))
+        raise Exception(msg.ImportError3 % (args[0], args[1], args[0].upper()))
 
-    # import module
+    # Import module
     module = import_module(full_dotted_name)
 
-    # extract class
+    # Extract class
     if hasattr(module, args[1]):
         return getattr(module, args[1])
     else:
-        raise Exception(msg.ImportError4 % 
-            (args[0], args[1], args[1]))
+        raise Exception(msg.ImportError4 % (args[0], args[1], args[1]))
 
 
 def tilde_expand(mydict):
-    """ Expands tilde character in path strings
     """
-    for key,val in mydict.items():
-        if type(val) not in [str, unicode]:
-            raise Exception('Expanded objects must be str or unicode')
-        if val[0:2] == '~/':
-            mydict[key] = os.getenv('HOME') +'/'+ val[2:]
+    Expands tilde character (~) in Path strings
+
+    :type mydict: dict
+    :param mydict: dictionary of paths to be expanded
+    """
+    for key, val in mydict.items():
+        if not isinstance(val, str):
+            raise Exception("Expanded objects must type: str")
+        elif val[:2] == '~/':
+            mydict[key] = f"{os.getenv('HOME')}/{val[2:]}"
+
     return mydict
 
 
-
-# utility functions
-
 def _par(key):
+    """
+    Utility function to return a global parameter
+
+    :type key: str
+    :param key: key to be passed to the parameter dict
+    :return: value for given key
+    """
     return sys.modules['seisflows_parameters'][key.upper()]
 
+
 def _path(key):
+    """
+    Utility function to return a global pathname
+
+    :type key: str
+    :param key: key to be passed to the parameter dict
+    :return: value for given key
+    """
     return sys.modules['seisflows_paths'][key.upper()]
 
+
 def _try(key):
+    """
+    Utility function to try to return a global parameter
+
+    :type key: str
+    :param key: key to be passed to the parameter dict
+    :return: value for given key or None
+    """
     try:
         return _par(key)
     except KeyError:
         return None
 
+
 def _output():
+    """
+    Utility function to return the full path to the output directory
+
+    :rtype: str
+    :return: full path to output directory
+    """
     try:
-        return _full(_path('output'))
-    except:
-        return _full(join('.', 'output'))
+        return _full(_path("output"))
+    except IOError:
+        return _full(os.path.join(".", "output"))
+
 
 def _full(path):
+    """
+    Utility function to return a full path with trailing '/'
+
+    :type path: str
+    :param path: path to be returned in full
+    :rtype: str
+    :return: full path
+    """
     try:
-        return join(abspath(path), '')
-    except:
+        return os.path.join(os.path.abspath(path), '')
+    except Exception as e:
         raise IOError
 
 
-# the following code changes how instance methods are handled by pickle.  placing it here, in this module, ensures that pickle changes will be in effect for all SeisFlows workflows
-
-# for relevant discussion, see stackoverflow thread "Can't pickle <type 'instancemethod'> when using python's multiprocessing Pool.map()"
-
 def _pickle_method(method):
+    """
+    The following code changes how instance methods are handled by pickle.
+    Placing it in this module ensures that pickle changes will be in
+    effect for all SeisFlows workflows
+
+    Note: For relevant discussion, see stackoverflow thread:
+    "Can't pickle <type 'instancemethod'> when using python's
+    multiprocessing Pool.map()"
+
+    Relevant Links (last accessed 01.20.2020):
+        https://stackoverflow.com/questions/7016567/
+        picklingerror-when-using-multiprocessing
+
+        https://bytes.com/topic/python/answers/
+        552476-why-cant-you-pickle-instancemethods
+    """
     func_name = method.im_func.__name__
     obj = method.im_self
     cls = method.im_class
@@ -230,6 +298,9 @@ def _pickle_method(method):
 
 
 def _unpickle_method(func_name, obj, cls):
+    """
+    The unpickling counterpart to the above function
+    """
     for cls in cls.mro():
         try:
             func = cls.__dict__[func_name]
@@ -240,5 +311,5 @@ def _unpickle_method(func_name, obj, cls):
     return func.__get__(obj, cls)
 
 
-copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
+copyreg.pickle(types.MethodType, _pickle_method, _unpickle_method)
 
