bb827aeb9fbd8d136454cefd9363d55f17177d68
Bryant Chow <bryant.h.c@gmail.com>
2020-01-22 12:35:05 +1300

    updated base workflow

--- seisflows/workflow/base.py
+++ seisflows/workflow/base.py
@@ -1,29 +1,33 @@
-
+#!/usr/bin/env python
+"""
+This is the Base class for seisflows.workflow.
+It contains mandatory functions that must be called by subclasses
+"""
 from seisflows.config import save
 
-class base(object):
-    """ Workflow abstract base class
+
+class Base(object):
+    """
+    Workflow abstract base class
     """
 
     def check(self):
-        """ Checks parameters and paths
         """
-        raise NotImplementedError('Must be implemented by subclass.')
-
-
+        Checks parameters and paths
+        """
+        raise NotImplementedError("Must be implemented by subclass.")
 
     def main(self):
-        """ Main routine
-
-          Execution of a workflow is equivalent to stepping through 
-          workflow.main
         """
-        raise NotImplementedError('Must be implemented by subclass.')
+        Main routine
 
+        Execution of a workflow is equivalent to stepping through workflow.main
+        """
+        raise NotImplementedError("Must be implemented by subclass.")
 
     def checkpoint(self):
-        """ Writes information to disk so workflow can be resumed following a
-          break
+        """
+        Writes information to disk so workflow can be resumed following a break
         """
         save()
 
