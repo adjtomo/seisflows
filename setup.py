81a713a0474cdede150f8eea07378c1bec63fd29
bch0w <bryant.h.c@gmail.com>
2021-10-29 01:57:10 +0000

    renaming first-layer directories from seisflows to seisflows3 to fully commit to the fork and avoid confusing with seisflows moving forward

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name="seisflows3",
       author="Seisflows Development Team",
       packages=find_packages(),
       entry_points={
-          "console_scripts": ["seisflows=seisflows.scripts.seisflows:main",]},
+          "console_scripts": ["seisflows=seisflows3.scripts.seisflows:main",]},
       license="GPL",
       zip_save=False
       )
