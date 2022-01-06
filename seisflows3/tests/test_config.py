d84b1c911ac22e544f958e05f453c723ad5d9719
Bryant Chow <bryant.h.c@gmail.com>
2022-01-05 17:45:30 -0900

    added various stages of parameter files for test data comprehensive testing for seisflows.config and seisflows command line tool, still a few functions to draw up tests for

--- seisflows3/tests/test_config.py
+++ seisflows3/tests/test_config.py
@@ -2,18 +2,126 @@
 Test the SeisFlows3 configuration script, which configures the compute
 system and the working environment required for SF3 to run properly
 """
+import os
 import sys
+import shutil
 import pytest
+from unittest.mock import patch
 from seisflows3 import config
-from seisflows3.scripts import SeisFlows
+from seisflows3.scripts.seisflows import SeisFlows
 
 
-def test_
+TEST_DIR = os.path.join(config.ROOT_DIR, "tests")
 
-def test_init_seisflows():
+
+@pytest.fixture
+def copy_par_file(tmpdir):
+    """
+    Copy the template parameter file into the temporary test directory
+    :rtype: str
+    :return: location of the parameter file
+    """
+    src = os.path.join(TEST_DIR, "test_data", "test_filled_parameters.yaml")
+    dst = os.path.join(tmpdir, "parameters.yaml")
+    shutil.copy(src, dst)
+
+
+@pytest.fixture
+def sfinit(tmpdir, copy_par_file):
+    """
+    Re-used function that will initate a SeisFlows3 working environment in
+    sys modules
+    :return:
+    """
+    copy_par_file
+    os.chdir(tmpdir)
+    with patch.object(sys, "argv", ["seisflows"]):
+        sf = SeisFlows()
+        sf._register(precheck=False)
+    config.init_seisflows()
+
+    return sf
+
+def test_seisflows_constants():
+    """
+    Ensure that the constants set in the Config file have not changed
+    Essentially a double check to make sure these things haven't been edited
+    because the rest of the package depends on these being accesible and
+    the same
+    """
+    names_check = ["system", "preprocess", "solver",
+                   "postprocess", "optimize", "workflow"]
+
+    packages_check = ["seisflows3", "seisflows3-super"]
+
+    root_dir_check = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), ".."
+    )
+
+    assert(config.NAMES == names_check)
+    assert(config.PACKAGES == packages_check)
+    assert(os.path.samefile(config.ROOT_DIR, root_dir_check))
+
+
+def test_init_seisflows(sfinit):
     """
     Make sure that initiation of the modular approach of seisflows works
     as expected. That is, that system-wide accessible modules are
     instantiated with the accepted naming schema
+
+    .. note::
+        This assumes that the parameter file is set up correctly, which it
+        should be if it's coming from the test data directory
     :return:
     """
+    sf = sfinit
+    # Ensure that all the modules in NAMES have been instantiated in sys.modules
+    for name in config.NAMES:
+        assert(f"seisflows_{name}" in sys.modules)
+
+
+def test_save_and_load(sfinit):
+    """
+    Test saving the current session to disk
+    :return:
+    """
+    # Instantiate sys modules and save to disk
+    sf = sfinit
+    config.save()
+    # Now remove seisflows sys modules so we can try load them back
+    for name in config.NAMES:
+        sys.modules.pop(f"seisflows_{name}")
+    config.load(path="./output")
+    for name in config.NAMES:
+        assert(f"seisflows_{name}" in sys.modules)
+
+
+def test_seisflows_paths_parameters():
+    """
+    Test the class that makes inputting and checking paths and parameters easier
+    """
+    # !!! TO DO
+    pass
+
+
+def test_custom_import(sfinit):
+    """
+    Test that importing based on internal modules works for various inputs
+    :return:
+    """
+    sfinit
+    with pytest.raises(SystemExit):
+        config.custom_import()
+    with pytest.raises(SystemExit):
+        config.custom_import(name="NOT A VALID NAME")
+
+    module = config.custom_import(name="optimize", module="LBFGS")
+    assert(module.__name__ == "LBFGS")
+    assert(module.__module__ == "seisflows3.optimize.LBFGS")
+
+    # Check one more to be safe
+    module = config.custom_import(name="optimize", module="steepest_descent")
+    assert(module.__name__ == "SteepestDescent")
+    assert(module.__module__ == "seisflows3.optimize.steepest_descent")
+
+
--- seisflows3/tests/test_seisflows.py
+++ seisflows3/tests/test_seisflows.py
@@ -1,6 +1,8 @@
 """
 Test suite for the SeisFlows command line interface tool and the underlying
 parser class, ensures that the command line tool works as expected
+
+!!! TO DO: Finish tests from 'edit' onwards
 """
 import os
 import io
@@ -9,82 +11,237 @@ import shutil
 import pytest
 import contextlib
 import subprocess
+from unittest.mock import patch
 from seisflows3.scripts.seisflows import sfparser, SeisFlows
-from seisflows3.config import Dict
+from seisflows3.config import Dict, ROOT_DIR
 from seisflows3.tools.tools import loadyaml
+
+TEST_DIR = os.path.join(ROOT_DIR, "tests")
 
 
 @pytest.fixture
-def par_file(tmpdir):
+def filled_par_file():
+    """A parameter file that is completely filled out and can be read in"""
+    return os.path.join(TEST_DIR, "test_data", "test_filled_parameters.yaml")
+
+
+@pytest.fixture
+def conf_par_file():
+    """A par file that has been configured but requires user-defined values"""
+    return os.path.join(TEST_DIR, "test_data", "test_conf_parameters.yaml")
+
+
+@pytest.fixture
+def setup_par_file():
+    """A barebones par file that only contains module names"""
+    return os.path.join(TEST_DIR, "test_data", "test_setup_parameters.yaml")
+
+
+@pytest.fixture
+def copy_par_file(tmpdir, filled_par_file):
     """
     Copy the template parameter file into the temporary test directory
     :rtype: str
     :return: location of the parameter file
     """
-    src = os.path.join("test_data", "test_parameters.yaml")
+    src = filled_par_file
     dst = os.path.join(tmpdir, "parameters.yaml")
     shutil.copy(src, dst)
-    return dst
 
 
 @pytest.fixture
-def par_file_dict(par_file):
+def par_file_dict(filled_par_file):
     """
     Return the test parameter file as a dictionary object
     :rtype: seisflows.config.Dict
     :return: dictionary of parameters
     """
-    return Dict(loadyaml(par_file))
+    return Dict(loadyaml(filled_par_file))
 
 
-@pytest.fixture
-def sf():
+def test_call_seisflows(tmpdir, par_file_dict, copy_par_file):
     """
-    Initiate an empty SeisFlows class which can be used for testing purposes
-    :return:
+    Test calling the 'par' command from command line and inside a python
+    environemnt. Check that case-insensitivity is also honored
+    Check against the actual value coming from the parameter file
+    Also tests the seisflows 'par' command
     """
-
-    sf = SeisFlows(return_self=True)
-
-
-def test_call_seisflows(tmpdir, par_file_dict):
-    """
-    Make sure that calling the SeisFlows class (i.e., __init__()) works for both
-    command line arguments and from within a Python environment
-    """
+    copy_par_file
     os.chdir(tmpdir)
-
-    # Test calling the 'par' command from command line and inside a python
-    # environemnt. Check that case-insensitivity is also honored
-    # Check against the actual value coming from the parameter file
     check_val = par_file_dict.LINESEARCH
-    for name in ["linesearch", "LINESEARCH", "LineSearch"]:
-        # From the command line
-        cmd_line_arg = ["seisflows", "par", name]
+
+    # Mock argv to match the actual scenario in which SeisFlows will be called
+    with patch.object(sys, "argv", ["seisflows"]):
+        for name in ["linesearch", "LINESEARCH", "LineSearch"]:
+            # From the command line
+            cmd_line_arg = ["seisflows", "par", name]
+            out = subprocess.run(cmd_line_arg, capture_output=True,
+                                 universal_newlines=True)
+            assert(out.stdout.strip() == f"{name.upper()}: {check_val}")
+
+            # Test from inside a Python environment; we need to redirect stdout to
+            # make sure the print statement is working as expected
+            f = io.StringIO()
+            with contextlib.redirect_stdout(f):
+                sf = SeisFlows()
+                sf(command="par", parameter=name)
+            stdout = f.getvalue()
+            assert(stdout.strip() == f"{name.upper()}: {check_val}")
+
+
+def test_edited_parameter_file_name(tmpdir, par_file_dict, filled_par_file):
+    """
+    Similar test as call_seisflows but just make sure that arbitrary naming
+    of the parameter file still works
+    """
+    par_fid = "CRAZY_PARAMETER_NAME-0x123kd.yaml"
+    par_name = "linesearch"
+
+    src = filled_par_file
+    dst = os.path.join(tmpdir, par_fid)
+    shutil.copy(src, dst)
+
+    os.chdir(tmpdir)
+    check_val = par_file_dict.LINESEARCH
+    cmd_line_arg = ["seisflows", "-p", par_fid, "par", par_name]
+    # Mock argv to match the actual scenario in which SeisFlows will be called
+    with patch.object(sys, "argv", ["seisflows"]):
         out = subprocess.run(cmd_line_arg, capture_output=True,
                              universal_newlines=True)
-        assert(out.stdout.strip() == f"{name.upper()}: {check_val}")
-
-        # Test from inside a Python environment:
-        # First we need to hackily substitute argv since our original command
-        # line arguments were running the tests
-        original_sys_argv = sys.argv
-        sys.argv = ["python"]
-        #  Then we need to redirect stdout to make sure the print statement is
-        #  working as expected
-        f = io.StringIO()
-        with contextlib.redirect_stdout(f):
-            SeisFlows(command="par", parameter=name)
-        stdout = f.getvalue()
-        assert(stdout.strip() == f"{name.upper()}: {check_val}")
-
-        # Reset sys.argv just incase we want to do anything else
-        sys.argv = original_sys_argv
+    assert(out.stdout.strip() == f"{par_name.upper()}: {check_val}")
 
 
-def test_register(tmpdir, par_file_dict):
+def test_register(tmpdir, par_file_dict, copy_par_file):
     """
     Test that the register function, which reads in PATHS and PARAMETERS
     works as expected, returning paths and parameters that we can read
     """
+    copy_par_file
+    os.chdir(tmpdir)
+
+    with patch.object(sys, "argv", ["seisflows"]):
+        sf = SeisFlows()
+        assert(sf._paths is None)
+        assert(sf._parameters is None)
+        sf._register(precheck=False)
+
+    # Check that paths and parameters have been set in sys.modules
+    paths = sys.modules["seisflows_paths"]
+    parameters = sys.modules["seisflows_parameters"]
+
+    # Check one or two parameters have been set correctly
+    assert(par_file_dict.LBFGSMAX == parameters.LBFGSMAX)
+    path_check_full = os.path.abspath(par_file_dict.PATHS["SCRATCH"])
+    assert(path_check_full == paths.SCRATCH)
+
+
+def test_cmd_modules():
+    """
+    Test module command which simply lists out the available (sub)modules
+    :return:
+    """
+    with patch.object(sys, "argv", ["seisflows"]):
+        f = io.StringIO()
+        with contextlib.redirect_stdout(f):
+            sf = SeisFlows()
+            sf(command="modules", name="workflow", package="seisflows3")
+        stdout = f.getvalue().strip().split()
+
+    # Assuming that these workflows will always be in the package
+    check_vals = ["base", "inversion", "migration"]
+    for check_val in check_vals:
+        assert(check_val in stdout)
+
+
+def test_cmd_setup(tmpdir):
+    """
+    Test setting up the SeisFlows3 working directory
+    """
+    os.chdir(tmpdir)
+    par_file = os.path.join(tmpdir, "parameters.yaml")
+
+    # Check with anmd without symlinking as well as with overwriting
+    with patch.object(sys, "argv", ["seisflows"]):
+        # Without symlinking
+        sf = SeisFlows()
+        sf.setup(symlink=False, overwrite=False)
+        assert(os.path.exists(par_file))
+        os.remove(par_file)
+
+        # With symlinking
+        sf.setup(symlink=True, overwrite=False)
+        assert(os.path.exists(par_file))
+        assert(os.path.exists(
+            os.path.join(tmpdir, "source_code", "seisflows3"))
+        )
+        # Edit the current par file in a noticeable way so we can check
+        # if overwriting works in the next step
+        test_phrase = "well this is rather unexpected...\n"
+        with open(par_file, "a") as f:
+            f.write(test_phrase)
+        with open(par_file, "r") as f:
+            assert(test_phrase in f.read())
+
+        # With overwriting
+        sf.setup(symlink=False, overwrite=True)
+        assert(os.path.exists(par_file))
+        with open(par_file, "r") as f:
+            text = f.read()
+            assert(test_phrase not in f.read())
+
+
+def test_cmd_configure(tmpdir, setup_par_file, conf_par_file):
+    """
+    Test configuring a parameter file
+    """
+    os.chdir(tmpdir)
+
+    # Copy in the setup par file so we can configure it
+    src = setup_par_file
+    dst = os.path.join(tmpdir, "parameters.yaml")
+    shutil.copy(src, dst)
+
+    # Configure the empty parameter file
+    with patch.object(sys, "argv", ["seisflows"]):
+        sf = SeisFlows()
+        # Need to run this via command line because there are sub arguments
+        cmd_line_arg = ["seisflows", "configure"]
+        subprocess.run(cmd_line_arg, capture_output=True,
+                       universal_newlines=True)
+
+    # We will be checking against an already configured par file
+    for check, par in zip(open(conf_par_file).readlines(),
+                          open(dst).readlines()):
+        # Titles will be difference since it's based on relative path
+        if "TITLE" in check:
+            continue
+        # Paths will also be different since they're relative
+        elif "PATH" in check:
+            break
+        # Otherwise all the lines should be the same
+        assert(check == par)
+
+
+def test_cmd_init(tmpdir, copy_par_file):
+    """
+    Test setting up the SeisFlows3 working directory
+    """
+    os.chdir(tmpdir)
+
+    with patch.object(sys, "argv", ["seisflows"]):
+        sf = SeisFlows()
+        sf.init()
+    pytest.set_trace()
+
+
+def blank(tmpdir):
+    """
+    Test setting up the SeisFlows3 working directory
+    """
+    pass
+    os.chdir(tmpdir)
+
+    with patch.object(sys, "argv", ["seisflows"]):
+        sf = SeisFlows()
+        sf._register(precheck=False)
 
