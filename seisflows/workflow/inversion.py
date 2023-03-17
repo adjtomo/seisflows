760d9ea6eb99b56a86d0dc318e57a9a5f1725677
bch0w <bryant.h.c@gmail.com>
2023-03-17 11:16:31 -0800

    removed optimize load checkpoint call in inversion setup because optimize setup already runs this functions so it was getting doubled up

--- seisflows/workflow/inversion.py
+++ seisflows/workflow/inversion.py
@@ -170,8 +170,6 @@ class Inversion(Migration):
         unix.mkdir(self.path.eval_func)
 
         self.optimize = self._modules.optimize  # NOQA
-        # If optimization has been run before, re-load from checkpoint
-        self.optimize.load_checkpoint()
 
     def run(self):
         """Call the forward.run() function iteratively, from `start` to `end`"""
