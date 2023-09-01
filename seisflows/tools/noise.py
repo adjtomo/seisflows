double checked rotation TT/RR adjoint sources back to N/E synthetics against Wang et al math. spent a good chunk of time convincing myself things were correct and feel confident that they are

seisflows/tools/noise.py
@@ -56,15 +56,21 @@ def rotate_ne_trace_to_rt(tr_ee, tr_ne, tr_en, tr_nn, theta, theta_p):
 
 def rotate_rt_adjsrc_to_ne(tr, theta, theta_p):
     """
-    Used during ambient noise adjoint tomography (workflow: noise inversion) to
-    rotate RR and TT adjoint sources back to N and E components for use in
-    N and E component adjoint simulations. `choice` allows User to select which
-    kernel (R or T) is being created, as each generates four adjoint sources.
+    Rotate RR and TT adjoint sources to N and E components for use in
+    N and E component adjoint simulations in ambient noise inversion workflow.
+
+    Paraphrasing Wang et al. (2019) 2.2.2
+
+    Kernels are generated from the time convolution of a forward field due to a
+    point force in the [east or north] direction, and an adjoint field obtained
+    by injecting [f_TT or f_RR (adjoint source)] in the east and north
+    directions at the receiver, with their amplitude modulated by the rotation
+    matrix.
 
     :type tr: obspy.core.trace.Trace
-    :param tr: input trace that is either R or T component. component
-        will be identified from Trace stats
-        :type theta: float
+    :param tr: input trace that is either R or T component. Component stat
+        of the Trace is required (Trace.stats.component)
+    :type theta: float
     :param theta: azimuth between source station and receiver station in units
         of radians
     :type theta_p: float
@@ -79,22 +85,29 @@ def rotate_rt_adjsrc_to_ne(tr, theta, theta_p):
     assert(component in ["R", "T"]), \
         "input adjoint source must have component 'R' or 'T'"
 
+    # Naming convention: AB (A=force source direction, B=injection direction)
+    # Copy the input trace to modify for the output traces
     tr_ee = tr.copy()
-    tr_ne = tr.copy()
     tr_en = tr.copy()
+    tr_ne = tr.copy()
     tr_nn = tr.copy()
 
     # TT rotation from Wang et al. (2019) Eq. 16
+    # Note that, confusingly, the rotation matrix notation order changes for
+    # w.r.t synthetics rotation ('en' switches with 'ne'), because the
+    # summation order changes for adjoint source rotation
     if component == "T":
-        tr_ee.data = +1 * np.cos(theta) * np.cos(theta_p) * tr.data
-        tr_en.data = -1 * np.cos(theta) * np.sin(theta_p) * tr.data
-        tr_ne.data = -1 * np.sin(theta) * np.cos(theta_p) * tr.data
-        tr_nn.data = +1 * np.sin(theta) * np.sin(theta_p) * tr.data
+        tr_ee.data = + 1 * np.cos(theta) * np.cos(theta_p) * tr.data  # K_ET
+        tr_en.data = - 1 * np.cos(theta) * np.sin(theta_p) * tr.data  # K_ET
+        tr_ne.data = - 1 * np.sin(theta) * np.cos(theta_p) * tr.data  # K_NT
+        tr_nn.data = + 1 * np.sin(theta) * np.sin(theta_p) * tr.data  # K_NT
     # TT rotation from Wang et al. (2019) Eq. 18
     elif component == "R":
-        tr_ee.data = +1 * np.sin(theta) * np.sin(theta_p) * tr.data
-        tr_en.data = +1 * np.sin(theta) * np.cos(theta_p) * tr.data
-        tr_ne.data = +1 * np.cos(theta) * np.sin(theta_p) * tr.data
-        tr_nn.data = +1 * np.cos(theta) * np.cos(theta_p) * tr.data
+        tr_ee.data = + 1 * np.sin(theta) * np.sin(theta_p) * tr.data  # K_ER
+        tr_en.data = + 1 * np.sin(theta) * np.cos(theta_p) * tr.data  # K_ER
+        tr_ne.data = + 1 * np.cos(theta) * np.sin(theta_p) * tr.data  # K_NR
+        tr_nn.data = + 1 * np.cos(theta) * np.cos(theta_p) * tr.data  # K_NR
 
+    # Return in order that all other functions are using, despite the rotation
+    # matrix having a different order
     return tr_ee, tr_ne, tr_en, tr_nn