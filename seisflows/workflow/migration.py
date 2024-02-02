b743db005ce2b80f545cf28b9f0a3032f521a2c7
Bryant Chow <bryant.h.c@gmail.com>
2024-02-01 17:39:15 -0900

    BUGFIX: missing quotes on log message

--- seisflows/workflow/migration.py
+++ seisflows/workflow/migration.py
@@ -198,8 +198,8 @@ class Migration(Forward):
                 # region so we modify the amplitude 
                 maskv = mask_model.vector
                 if self.solver.scale_mask_region:
-                    logger.info(f"scaling source mask by 
-                                {self.solver.scale_mask_region}")
+                    logger.info(f"scaling source mask by "
+                                f"{self.solver.scale_mask_region}")
                     maskv[maskv < 1] *= self.solver.scale_mask_region  
 
                 # Need to expand vector the length of the event kernel vector
