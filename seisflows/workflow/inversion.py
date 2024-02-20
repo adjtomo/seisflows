9a58be4fe54cb682d488db2415df614439dacdb9
Bryant Chow <bryant.h.c@gmail.com>
2024-02-20 11:54:28 -0900

    misc change inversion

--- seisflows/workflow/inversion.py
+++ seisflows/workflow/inversion.py
@@ -469,6 +469,8 @@ class Inversion(Migration):
         # and is required for the line search 
         logger.info("calculating search direction `p_new` from gradient")
         p_new = self.optimize.compute_direction()
+
+        # Save optimization updates to disk for restarts/checkpoint
         self.optimize.checkpoint()
         self.optimize.save_vector(name="p_new", m=p_new)
 
